c800a4775c23bbdb701125e805bfe1a7c7929163
KRKRKRKRKRKRKRKRKRKR <k025g0083@g.neec.ac.jp>
2026-04-17 14:17:51 +0900

    00_04 ログを出そう

--- main.cpp
+++ main.cpp
@@ -78,19 +78,42 @@ void Log(const std::string& message) {
 }
 
 //文字列変換関数
-std::wstring ConvertString(const std::string& str);
-std::string ConvertString(const std::wstring& wstr);
+std::wstring ConvertString(const std::string& str) {
+	if (str.empty()) {
+		return std::wstring();
+	}
 
+	auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
+	if (sizeNeeded == 0) {
+		return std::wstring();
+	}
+	std::wstring result(sizeNeeded, 0);
+	MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
+	return result;
+}
+
+std::string ConvertString(const std::wstring& str) {
+	if (str.empty()) {
+		return std::string();
+	}
+
+	auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
+	if (sizeNeeded == 0) {
+		return std::string();
+	}
+	std::string result(sizeNeeded, 0);
+	WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
+	return result;
+}
 
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 	CreateGameWindow();
 
 	int enemyHP = 100;
-	Log(std::format("enemyHP{}\n", enemyHP));
+	Log(std::format("enemyHP {}\n", enemyHP));
 
-//	Log(ConvertString(std::format(L"WSTRING{}\n",enemyHP)));
-
+	Log(ConvertString(std::format(L"WSTRING {}\n", enemyHP)));
 	MSG msg{};
 	//メッセージループ
 	while (msg.message != WM_QUIT) {
