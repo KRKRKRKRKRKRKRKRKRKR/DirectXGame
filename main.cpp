379ada940f917cb892820cf46488f9d7d1c4da10
KRKRKRKRKRKRKRKRKRKR <k025g0083@g.neec.ac.jp>
2026-04-17 15:28:09 +0900

    00_05

--- main.cpp
+++ main.cpp
@@ -1,10 +1,193 @@
 #include <Windows.h>
 
-int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE,_In_ LPSTR, _In_ int) {
-	
-	//デバックテキストの生成
-	OutputDebugStringA("Hello DirectX\n");
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <cassert>
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
+
+//ウィンドウプロシージャ
+LRESULT CALLBACK windowPrec(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
 
 	return 0;
 
+	//標準のメッセージ処理
+	return DefWindowProc(hwnd, msg, wparam, lparam);
 }
+
+
+/// <summary>
+/// ウィンドウの作成
+/// </summary>
+void CreateGameWindow() {
+
+	//ウィンドウクラスの登録
+	WNDCLASS wc{};
+
+	//ウィンドウプロシージャー
+	wc.lpfnWndProc = windowPrec;
+
+	//クラス名
+	wc.lpszClassName = L"DirectXGame";
+
+	//インスタンスハンドル
+	wc.hInstance = GetModuleHandle(nullptr);
+
+	//カーソル
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+
+	//ウィンドウクラスを登録する
+	RegisterClass(&wc);
+
+	//ウィンドウサイズ
+	const int32_t kClientWidth = 1280;
+	const int32_t kClientHeight = 720;
+
+	//ウィンドウサイズを表す構造体にクライアント領域を入れる
+	RECT wrc = { 0, 0, kClientWidth, kClientHeight };
+
+	//クライアント領域を元に実際のサイズに変換する
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+	//ウィンドウの作成
+	HWND hwnd = CreateWindow(
+		wc.lpszClassName,		//クラス名
+		L"DirectXGame",			//タイトルバーの文字
+		WS_OVERLAPPEDWINDOW,	//ウィンドウスタイル
+		CW_USEDEFAULT,			//表示X座標
+		CW_USEDEFAULT,			//表示Y座標
+		wrc.right - wrc.left,	//ウィンドウ幅
+		wrc.bottom - wrc.top,	//ウィンドウ高
+		nullptr,				//親ウィンドウハンドル
+		nullptr,				//メニューハンドル
+		wc.hInstance,			//インスタンスハンドル
+		nullptr					//追加パラメーター
+	);
+
+	//ウィンドウの表示
+	ShowWindow(hwnd, SW_SHOW);
+}
+
+void Log(const std::string& message) {
+	OutputDebugStringA(message.c_str());
+}
+
+//文字列変換関数
+std::wstring ConvertString(const std::string& str) {
+	if (str.empty()) {
+		return std::wstring();
+	}
+
+	auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
+	if (sizeNeeded == 0) {
+		return std::wstring();
+	}
+	std::wstring result(sizeNeeded, 0);
+	MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
+	return result;
+}
+
+std::string ConvertString(const std::wstring& str) {
+	if (str.empty()) {
+		return std::string();
+	}
+
+	auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
+	if (sizeNeeded == 0) {
+		return std::string();
+	}
+	std::string result(sizeNeeded, 0);
+	WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
+	return result;
+}
+
+
+
+
+
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+
+	CreateGameWindow();
+
+	int enemyHP = 100;
+	Log(std::format("enemyHP {}\n", enemyHP));
+
+	Log(ConvertString(std::format(L"WSTRING {}\n", enemyHP)));
+	MSG msg{};
+
+	//DXGIファクトリーの生成
+	IDXGIFactory7* dxgiFactory = nullptr;
+
+	HRESULT hr = CreateDXGIFactory(IID_PPV_ARGS(&dxgiFactory));
+	assert(SUCCEEDED(hr));
+
+	//使用するアダプタ用の変数
+	IDXGIAdapter4* useAdapter = nullptr;
+
+	for (UINT i = 0; dxgiFactory->EnumAdapterByGpuPreference(i,
+		DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&useAdapter)) !=
+		DXGI_ERROR_NOT_FOUND; ++i) {
+
+		//アダプタの情報を取得する
+		DXGI_ADAPTER_DESC3 adapterDesc{};
+		hr = useAdapter->GetDesc3(&adapterDesc);
+		assert(SUCCEEDED(hr));
+
+		if (!(adapterDesc.Flags & DXGI_ADAPTER_FLAG3_SOFTWARE)) {
+			Log(ConvertString(std::format(L"use Adapter:{}\n", adapterDesc.Description)));
+			break;
+		}
+		useAdapter = nullptr;
+	}
+
+	assert(useAdapter != nullptr);
+
+	ID3D12Device* device = nullptr;
+
+	D3D_FEATURE_LEVEL featureLevels[] = {
+		D3D_FEATURE_LEVEL_12_2, D3D_FEATURE_LEVEL_12_1,D3D_FEATURE_LEVEL_12_0
+	};
+
+	const char* featureLevelStrings[] = {
+		"12.2", "12.1", "12.0"
+	};
+
+	for (size_t i = 0; i < _countof(featureLevels); i++) {
+
+		hr = D3D12CreateDevice(useAdapter, featureLevels[i], IID_PPV_ARGS(&device));
+
+		if (SUCCEEDED(hr)) {
+			Log(std::format("FeatureLevel:{}\n", featureLevelStrings[i]));
+			break;
+		}
+	}
+
+	assert(device != nullptr);
+	Log("Complete create D3D12Device\n");
+
+
+	//メッセージループ
+	while (msg.message != WM_QUIT) {
+		//windowからのメッセージがあるかどうかをチェックする
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+
+		}
+		else {
+			//ゲームの処理
+
+		}
+	}
+
+	return 0;
+
+
+}
+
+
+
+
+
+
