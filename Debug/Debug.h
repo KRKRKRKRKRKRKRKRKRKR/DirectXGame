05b267f02053e266439bb9cacbfb4bd81e08c1d0
k025g0083Kojimaryusei <k025g0083@g.neec.ac.jp>
2026-05-11 02:13:13 +0900

    READMEの追加

---
+++ Debug/Debug.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <Windows.h>
+#include <dbghelp.h>
+#include <strsafe.h>
+#include <d3d12.h>
+#include <d3d12sdklayers.h>
+#include <dxgi1_6.h>
+#include <dxgidebug.h>
+#pragma comment(lib, "dbghelp.lib")
+
+class Debug {
+	public:
+	Debug() = delete;
+
+	// 例外フィルタを登録する	
+	static void RegisterCrashHandler();
+
+	// デバッグレイヤーを有効にする
+	static void EnableDebugLayer();
+
+	//InfoQueueを取得する
+	static void SetupInfoQueue(ID3D12Device* dx);
+
+	// ライブオブジェクトをレポートする
+	static void ReportLiveObjects();
+
+private:
+	// クラッシュ時に呼ばれるコールバック
+	static LONG WINAPI ExportDump(EXCEPTION_POINTERS* exception);
+};
+
